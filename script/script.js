ea8bfc11a4ef3e27735aec2cea8c6ceb44534211
Danila Karavaev <exir74@gmail.com>
2023-01-14 11:40:30 +0500

    feat: создать попап

--- script/script.js
+++ script/script.js
@@ -3,54 +3,33 @@ let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close');
 let popupForm = document.querySelector('.popup__form');
 let profileInfo = document.querySelector('.profile-info');
-let profileName = profileInfo.querySelector('.profile-info__name');
-let profileSubtitle = profileInfo.querySelector('.profile-info__subtitle');
-let profileNamePopup = document.querySelector('.popup__name')
+let profileName = document.querySelector('.profile-info__name');
+let profileSubtitle = document.querySelector('.profile-info__subtitle');
+let profileNamePopup = document.querySelector('.popup__name');
 let profileSubtitlePopup = document.querySelector('.popup__subtitle');
 let popupSaveButton = document.querySelector('.popup__button');
 
-
-function popupOpener(){
+function popupOpener() {
   popup.classList.remove('popup_close');
   popup.classList.add('popup_open');
   // qq = profileName.textContent;
   // profileNamePopup.setAttribute('value', qq);
-  // profileNamePopup.setAttribute('value', profileName.textContent);
-  // console.log(profileNamePopup);
-  // console.log(profileSubtitlePopup);
-
+  profileNamePopup.setAttribute('value', profileName.textContent);
+  profileSubtitlePopup.setAttribute('value', profileSubtitle.textContent);
 }
-// profileName.innerHTML= (profileNamePopup.getAttribute('value'));
 
-
-
-function popupClose(){
+function popupClose() {
   popup.classList.remove('popup_open');
   popup.classList.add('popup_close');
 }
 
-function popupSavaClose(){  
+function popupSavaClose() {
   popup.classList.remove('popup_open');
   popup.classList.add('popup_close');
-console.log(profileName.textContent)
-console.log(profileNamePopup.getAttribute('type', 'text'))
+  profileName.textContent = profileNamePopup.value;
+  profileSubtitle.textContent = profileSubtitlePopup.value;
 }
 
 popupButton.addEventListener('click', popupOpener);
 popupCloseButton.addEventListener('click', popupClose);
 popupSaveButton.addEventListener('click', popupSavaClose);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
