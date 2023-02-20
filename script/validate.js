a4547c144c788776df29c13cd6ce0ab689df8476
Exir74 <exir74@gmail.com>
2023-02-20 17:08:35 +0500

    feat: создать разметку для ошибки валидации re

--- script/validate.js
+++ script/validate.js
@@ -1,11 +1,19 @@
-// включение валидации вызовом enableValidation
-// все настройки передаются при вызове
-
+// const formList = document.querySelectorAll('.popup__form');
+// const inputSelector = document.querySelectorAll('.popup__input') ;
+// const submitButtonSelector = document
+// function enableValidation(){};
 // enableValidation({
-//   formSelector: '.popup__form',
+//   formSelector: Array.from(document.querySelectorAll('.popup__form')),
 //   inputSelector: '.popup__input',
 //   submitButtonSelector: '.popup__button',
 //   inactiveButtonClass: 'popup__button_disabled',
 //   inputErrorClass: 'popup__input_type_error',
 //   errorClass: 'popup__error_visible'
 // });
+// console.log(formList)
+
+const enableValidation = () =>{
+  const formList = Array.from(document.querySelectorAll('.popup__form'))
+  console.log(formList)
+}
+enableValidation()
