1dcb7132f5624a92afbf39505067ba6ae154b018
Exir74 <exir74@gmail.com>
2023-02-20 14:37:14 +0500

    feat: создать разметку для ошибки валидации

--- script/validate.js
+++ script/validate.js
@@ -1,8 +1,19 @@
-// enableValidation({
-//   formSelector: '.popup__form',
-//   inputSelector: '.popup__input',
-//   submitButtonSelector: '.popup__button',
-//   inactiveButtonClass: 'popup__button_disabled',
-//   inputErrorClass: 'popup__input_type_error',
-//   errorClass: 'popup__error_visible'
-// }); 
+const formSelector = document.querySelector('.popup__form');
+const inputSelector = form.querySelector('.popup__input');
+const formError = form.querySelector(`.${inputSelector.id}-error`);
+const submitButtonSelector = form.querySelector('.popup__button')
+const inactiveButtonClass = form.querySelector('popup__button_error')
+// const inputErrorClass: 'popup__input_type_error',
+
+
+
+
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_error',
+  errorClass: 'popup__error_visible'
+}); 
