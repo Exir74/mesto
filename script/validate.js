0a5a3d903f8c1f8db549280371c195bcf9252250
Exir74 <exir74@gmail.com>
2023-02-21 20:26:44 +0500

    feat: создать разметку для ошибки валидации

--- script/validate.js
+++ script/validate.js
@@ -1,9 +1,43 @@
-
+// const formSelector = document.querySelector('.popup__form');
+// const inputSelector = formSelector.querySelector('.popup__input');
+// const submitButtonSelector =
+//   formSelector.querySelector('.popup__button');
+// const inactiveButtonClass = formSelector.querySelector(
+//   '.popup__button_disabled'
+// );
+// const inputErrorClass = formSelector.querySelector(
+//   '.popup__input_type_error'
+// );
+// const errorClass = formSelector.querySelector(
+//   '.popup__error_visible'
+// );
+// function enableValidation() {
+//}
+// console.log(formSelector)
 enableValidation({
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__button',
   inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error', это класс  измененного INPUT
-  errorClass: 'popup__error_visible' это будет класс спана 
-}); 
+  inputErrorClass: 'popup__input_type_error',
+  //это класс  измененного INPUT
+  errorClass: 'popup__error_visible'
+  //это будет класс спана
+});
+
+
+
+// inputSelector.addEventListener('input', function (evt) {
+//   // Выведем в консоль значение свойства validity.valid поля ввода, 
+//   // на котором слушаем событие input
+//   // console.log(evt.target.validity.valid);
+// }); 
+// const iterateForm = ()=> {
+//   Array.from(formSelector).forEach((item) => {
+//     console.log(item)
+//     item.addEventListener('input', (evt)=>{
+//       console.log(evt.target.validity.valid);
+//     })
+//   })
+// }
+// iterateForm(formSelector)
