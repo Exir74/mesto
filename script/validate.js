f5b0b2b18dbdd4132b419d4be6691e08505b430b
Exir74 <exir74@gmail.com>
2023-02-21 12:27:40 +0500

    feat: создать разметку для ошибки валидации re

--- script/validate.js
+++ script/validate.js
@@ -1,19 +1,30 @@
-// const formList = document.querySelectorAll('.popup__form');
-// const inputSelector = document.querySelectorAll('.popup__input') ;
-// const submitButtonSelector = document
-// function enableValidation(){};
-// enableValidation({
-//   formSelector: Array.from(document.querySelectorAll('.popup__form')),
-//   inputSelector: '.popup__input',
-//   submitButtonSelector: '.popup__button',
-//   inactiveButtonClass: 'popup__button_disabled',
-//   inputErrorClass: 'popup__input_type_error',
-//   errorClass: 'popup__error_visible'
-// });
-// console.log(formList)
+// Вынесем все необходимые элементы формы в константы
+const formElement = document.querySelector('.popup__form');
+const formInput = formElement.querySelector('.popup__input');
+const span = formElement.querySelector('.popup__span-error')
 
-const enableValidation = () =>{
-  const formList = Array.from(document.querySelectorAll('.popup__form'))
-  console.log(formList)
-}
-enableValidation()
+// Функция, которая добавляет класс с ошибкой
+const showInputError = (element) => {
+  element.classList.add('popup__input-error');
+  span.classList.add('popup__error_visible')
+};
+
+// Функция, которая удаляет класс с ошибкой
+const hideInputError = (element) => {
+  element.classList.remove('popup__input-error');
+  span.classList.remove('popup__error_visible')
+};
+
+// Функция, которая проверяет валидность поля
+const isValid = () => {
+  if (!formInput.validity.valid) {
+    // Если поле не проходит валидацию, покажем ошибку
+    showInputError(formInput);
+  } else {
+    // Если проходит, скроем
+    hideInputError(formInput);
+  }
+};
+
+// Вызовем функцию isValid на каждый ввод символа
+formInput.addEventListener('input', isValid); 
