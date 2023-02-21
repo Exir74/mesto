4912ae9b250f0a4b9bc0708fd881d9746d141fe7
Exir74 <exir74@gmail.com>
2023-02-21 14:28:55 +0500

    feat: создать разметку для ошибки валидации

---
+++ script/validate.js
@@ -0,0 +1,9 @@
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error', это класс  измененного INPUT
+  errorClass: 'popup__error_visible' это будет класс спана 
+}); 
