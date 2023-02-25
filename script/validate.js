30b96d94818f97ff05f6133171378f0b28fe8d33
Danila Karavaev <exir74@gmail.com>
2023-02-25 16:18:48 +0500

    feat: добавить span для валидации форм

--- script/validate.js
+++ script/validate.js
@@ -79,8 +79,8 @@ const formValidationConfig = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
   inputErrorClass: 'popup__input_type_error',
-  inactiveButtonClass: 'popup__button_inactive',
 };
 
 function disableSubmit(event) {
