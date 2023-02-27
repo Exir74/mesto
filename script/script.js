1eca34ea1237014852c7075f53f69e683d4a7fbf
Exir74 <exir74@gmail.com>
2023-02-27 11:50:36 +0500

    bugfix

--- script/script.js
+++ script/script.js
@@ -32,32 +32,7 @@ const buttonAddImage = page.querySelector('.profile__add-button');
 const buttonEditProfile = page.querySelector('.profile__edit-button');
 const cards = page.querySelector('.cards');
 const card = page.querySelector('.card');
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-];
+
 renderInitialCards();
 //создание карточки
 function createCard(cardLink, cardName) {
