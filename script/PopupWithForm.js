cb0d2ce1935fffd10b040bfe804dbb520ad80612
Danila Karavaev <exir74@gmail.com>
2023-03-28 23:20:55 +0500

    poppup open

--- script/PopupWithForm.js
+++ script/PopupWithForm.js
@@ -4,27 +4,31 @@ export class PopupWithForm extends Popup {
   constructor(popupSelector, { hedlerPopupForm }) {
     super(popupSelector);
     this._hedlerPopupForm = hedlerPopupForm;
+    // console.log(this);
   }
   open() {
     super.open();
-    this.setEventListeners()
+    this._hedlerPopupFor
+    // this.setEventListeners()
     
   }
   close() {
     super.close();
   }
   setEventListeners(){
-    this._hedlerPopupForm(this._getInputValues());
-    this._getInputValues()
+    super.setEventListeners()
+    // this._hedlerPopupForm(this._getInputValues());
+    this._popupSelector.querySelector
+    // this._getInputValues()
   }
-  _getInputValues() {
-    this._inputList =
-      this._popupSelector.querySelectorAll('.popup__input');
-    this._formValues = {};
-    console.log(this._formValues);
-    this._inputList.forEach((input) => {
-      this._formValues[input.name] = input.value;
-      return this._formValues;
-    });
-  }
+  // _getInputValues() {
+  //   this._inputList =
+  //     this._popupSelector.querySelectorAll('.popup__input');
+  //   this._formValues = {};
+  //   console.log(this._formValues);
+  //   this._inputList.forEach((input) => {
+  //     this._formValues[input.name] = input.value;
+  //     return this._formValues;
+  //   });
+  // }
 }
