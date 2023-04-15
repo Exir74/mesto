7a4b602609052efda08e5b2514081641511f0830
Danila Karavaev <exir74@gmail.com>
2023-04-15 12:08:31 +0500

    add avatar

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,5 +1,5 @@
 export class Section {
-  constructor({ data, renderer, }, containerSelector) {
+  constructor({ data, renderer }, containerSelector) {
     this._renderedItems = data;
     this._container = document.querySelector(containerSelector);
     this._renderer = renderer;
